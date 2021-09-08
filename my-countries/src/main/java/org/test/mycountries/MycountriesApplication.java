e39f8513617866cb67cec697f25c1b8798087f1a
tharanga <dtbwije@gmail.com>
2021-09-08 09:10:44 -0700

    Initial maven setup completed.

---
+++ my-countries/src/main/java/org/test/mycountries/MycountriesApplication.java
@@ -0,0 +1,13 @@
+package org.test.mycountries;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MycountriesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MycountriesApplication.class, args);
+	}
+
+}
