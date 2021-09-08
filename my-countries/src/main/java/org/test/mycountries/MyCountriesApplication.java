08a7be8bb335a9da9f13373624b7af560b51a4a6
tharanga <dtbwije@gmail.com>
2021-09-08 09:41:29 -0700

    Sprint application is working. Poms updated to include sub-sub modules. Application is runnable.

--- my-countries/src/main/java/org/test/mycountries/MyCountriesApplication.java
+++ my-countries/src/main/java/org/test/mycountries/MyCountriesApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MycountriesApplication {
+public class MyCountriesApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MycountriesApplication.class, args);
+		SpringApplication.run(MyCountriesApplication.class, args);
 	}
 
 }
